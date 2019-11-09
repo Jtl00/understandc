22d18d8426a2d769b8a0864f8011ec79eadb7fa0
edz <edz@yxdeMacBook-Air.local>
2019-11-09 18:33:22 +0800

    学生管理(创建删除)

--- studentdemo1.c
+++ studentdemo1.c
@@ -30,5 +30,25 @@ Student *createStudent(int age, enum gender sex, char *name, int num) {
     return rlt;
 }
 void displayStudent(Student *stu) {
-    printf("学号: %d,姓名: %s,年龄:%d,性别: %s\n", stu->num, stu->name, stu->age, stu->sex == female ? "女":"男");
+    printf("学号: %d,姓名: %s,年龄:%d,性别: %s\n", stu->num, stu->name, stu->age, stu->sex == female ? "女" : "男");
+}
+Student *search(Student *studentPArr[], int num) {
+    for (int j = 1; studentPArr[j] != NULL; j++) {
+        if (studentPArr[j]->num == num) {
+            printf("%s\n", studentPArr[j]->name);
+        }
+
+    }
+
+    return NULL;
+
+}
+void deleteStudentByNum(Student *stu[], Student *student) {
+    for (int i = 1; stu[i] != NULL; i++) {
+        if (stu[i]->num == student->num) {
+        }
+
+        stu[i] = stu[i + 1];
+        printf("学号: %d,姓名: %s,年龄:%d,性别: %s\n", stu[i]->num, stu[i]->name, stu[i]->age, stu[i]->sex == female ? "女" : "男");
+    }
 }
